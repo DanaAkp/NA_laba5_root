e527aefa45b85fdf8e7d0a35f9a90c5325ff8127
Danagul Akpaeva <aidanagul10@gmail.com>
2019-05-16 22:51:52 +0300

    что-то не так с методом хорд и простых итераций

--- NA_laba5/Program.cs
+++ NA_laba5/Program.cs
@@ -35,6 +35,12 @@ namespace NA_laba5
 
             Console.WriteLine("Метод ньютона");
             Console.WriteLine(Clarification_Newton(a1, b1, epsilon));
+
+            Console.WriteLine("Метод хорд ");
+            Console.WriteLine(Clarification_Chorda(a1, b1, epsilon));
+
+            Console.WriteLine("Метод простых итераций");
+            Console.WriteLine(Clarification_Iter(a1, b1, epsilon));
             Console.ReadLine();
         }
         public static double Func1(double x)
@@ -44,11 +50,8 @@ namespace NA_laba5
         }
         public static double Func2(double x)
         {
-            return x - Math.Cos(x);
-        }
-        public static double Func3(double x)
-        {
-            return 2 * Math.Atan(x) - 3 * x + 2;
+            // return x - Math.Cos(x);
+            return (4 * x / 3) - (Math.Pow(x, 3) / 12) - (1 / 6);
         }
 
         public static double func1_Proizz(double x)
@@ -74,23 +77,24 @@ namespace NA_laba5
         }
         public static string Clarification_Bisection(double a, double b, double epsilon)
         {
-            string s = "a\tc\tb\tf(a)\tf(c)\tf(b)\t|b-a|\n";
+            string s = "n\ta\tc\tb\tf(a)\tf(c)\tf(b)\t|b-a|\n";
+            int count = 0;
             while (b - a > epsilon)
             {
                 double c = (a+b) / 2;
-                s +=Math.Round( a,4) + "\t" + Math.Round(c, 4) + "\t" + Math.Round(b, 4) + "\t";
+                s +=count+"\t"+ Math.Round( a,4) + "\t" + Math.Round(c, 4) + "\t" + Math.Round(b, 4) + "\t";
                 double f_c = Func1(c);
                 if (f_c == 0) break; 
                 else
                 {
                     double f_a = Func1(a);
                     double f_b = Func1(b);
-                    s += Math.Round(a, 4) + "\t" + Math.Round(c, 4) + "\t" + Math.Round(b,4) + "\t";
+                    s += count + Math.Round(a, 4) + "\t" + Math.Round(c, 4) + "\t" + Math.Round(b,4) + "\t";
                     if (f_c * f_a > 0) a = c;
                     if (f_c * f_b > 0) b = c;
                 }
 
-                s += (b -a).ToString() + "\n";
+                s += (b - a).ToString() + "\n";count++;
             }
             s += "Корень равен " + (a + b) / 2;
             return s;
@@ -117,20 +121,46 @@ namespace NA_laba5
         }
         public static string Clarification_Chorda(double a, double b,double epsilon)
         {
-            string s = "n\tx\tf(x)\t|Xn+1 - Xn|\n";
-
+            string s = "n\tx0\tx2\tx1\t|Xn - Xn-1|\n";
+            double x0 = a;
+            double x1 = b;
+            double x2 = new_Xn(x1, x0);
             double f_x = Func1(a);
-            //s += "0\t" + a + "\t" + f_x + "\t" + (f_x - a).ToString() + "\n";
+            s += "0\t" + x0 + "\t" + x2 + "\t" + x1 + "\t" + (x1-x2).ToString() + "\n";
             int c = 1;
-            while (f_x - a > epsilon)
+            while (x1 - x2 > epsilon)
             {
-                //s += c + "\t" + a + "\t" + f_x + "\t" + (f_x - a).ToString() + "\n";
-                a = f_x;
-                f_x = Func1(a); c++;
+                x0 = x1;
+                x1 = x2;
+                x2 = new_Xn(x1, x0);
+                s += c + "\t" + x0 + "\t" + x2 + "\t" + x1 + "\t" + (x1 - x2).ToString() + "\n";
+                c++;
             }
-           
-            
-            //s += "Корень равен " + (f_x - a) / 2;
+            s += "Корень равен " + x2;
+            return s;
+        }
+        public static double new_Xn(double Xn, double Xn_1)
+        {
+            return Xn - Func1(Xn) * (Xn_1 - Xn) / (Func1(Xn_1) - Func1(Xn));
+        }
+
+        public static string Clarification_Iter(double a, double b, double epsilon)
+        {
+            if (Math.Abs( Func2(a)) < 1 && Math.Abs( Func2(b)) < 1) return "Не выполняется основное условие сходимости";
+
+            string s = "n\tx0\tx1\t|Xn - Xn-1|\n";
+            double x0 = a;
+            double x1 = Func2(x0);
+            s += "0\t" + x0 + "\t" + x1 + "\t" + (x1 - x0).ToString() + "\n";
+            int c = 1;
+            while (x1-x0 > epsilon)
+            {
+                x0 = x1;
+                x1 = Func2(x0);
+                s += c + "\t" + x0 + "\t"  + x1 + "\t" + (x1 - x0).ToString() + "\n";
+                c++;
+            }
+            s += "Корень равен " + x1;
             return s;
         }
     }
